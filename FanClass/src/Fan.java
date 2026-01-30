6d746255c732dbd985084392cf60e2fb9e3f57bf
cleith20 <cameronjleith@gmail.com>
2026-01-30 13:06:39 -0700

    FanClass

---
+++ FanClass/src/Fan.java
@@ -0,0 +1,83 @@
+
+public class Fan {
+	//Data Members
+	private final int SLOW = 1;
+	private final int MEDIUM = 2;
+	private final int FAST = 3;
+	private int speed = SLOW;
+	private boolean on = false;
+	private double radius = 5.0;
+	private String color = "blue";
+	
+	private int id;
+	static private int count = 0;
+	
+	//Constructors
+	public Fan() {
+		count++;
+		id = count;
+	}
+	
+
+	public Fan(int speed, boolean on, double radius, String color) {
+		super();
+		this.speed = speed;
+		this.on = on;
+		this.radius = radius;
+		this.color = color;
+		count++;
+		id = count;
+		
+	}
+	public int getId() {
+		return id;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public boolean isOn() {
+		return on;
+	}
+
+	public void setOn(boolean on) {
+		this.on = on;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	@Override
+	public String toString() {
+		if(on = true) {
+		return "ID = " + id + "\nFan speed = " + speed 
+				+ "\nRadius = " + radius + "\nColor = " + color + "\nThe fan is on :)";
+		}
+		else {
+			return "ID = " + id + "\nRadius = " + radius + "\nColor = " + color + "\nThe fan is off";
+		}
+	}
+	
+	
+	
+	
+
+}
---
+++ FanClass/src/TestFan.java
@@ -0,0 +1,17 @@
+
+public class TestFan {
+
+	public static void main(String[] args) {
+		Fan fan1 = new Fan(3, true, 10.0, "yellow");
+		System.out.println(fan1);
+		
+		
+		System.out.println();
+		
+		Fan fan2 = new Fan(2, false, 5.0, "blue");
+		System.out.println(fan2);
+		
+
+	}
+
+}
