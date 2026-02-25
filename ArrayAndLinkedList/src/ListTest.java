15b1057eeca55154c923f8c963f59fa609c6ce4d
cleith20 <cameronjleith@gmail.com>
2026-02-24 22:12:25 -0700

    Lists

--- ArrayAndLinkedList/src/ListTest.java
+++ ArrayAndLinkedList/src/ListTest.java
@@ -5,28 +5,31 @@ public class ListTest {
 
 	public static void main(String[] args) {
 		
-		List<Integer> myArrList = new MyArrayList<>();
+		List<Integer> myArrList = new MyDoubleLinkedList<>();
 
 		myArrList.add(1);
-		myArrList.add(2);
-		myArrList.add(3);
 		myArrList.add(4);
-		myArrList.add(5);
+		myArrList.add(9);
+		myArrList.add(7);
+		myArrList.add(9);
 	
+		System.out.println(myArrList.lastIndexOf(9));
+		System.out.println(myArrList.indexOf(7));
+		System.out.println(myArrList.contains(1));
 		for(int i = 0; i < myArrList.size(); i++) {
 			System.out.print(myArrList.get(i) + " ");
 		}
-		System.out.println();
-		for(Integer num : myArrList) {
-			System.out.print(num + " ");
-		}
-		System.out.println();
-		
-		Iterator iter = myArrList.iterator();
-		while(iter.hasNext()) {
-			Integer ans = (Integer)iter.next();
-			System.out.print(ans + " ");
-		}
+//		System.out.println();
+//		for(Integer num : myArrList) {
+//			System.out.print(num + " ");
+//		}
+//		System.out.println();
+//		
+//		Iterator iter = myArrList.iterator();
+//		while(iter.hasNext()) {
+//			Integer ans = (Integer)iter.next();
+//			System.out.print(ans + " ");
+//		}
 	}
 
 }
